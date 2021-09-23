f3c61d4fa131ca106b7271f2e38276399bd3c1d5
Nalini <hiitsenlly@gmail.com>
2021-09-23 22:18:26 +0530

    My first TDD Kata1 commit

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,28 +1,25 @@
+/******Attempting Kata 1-String Calculator via ********/
+ /***** TDD approach *******/
+
 public class StringCalculator {
-	
+	//method to add 2 strings
 	 int Add(String Numbers) {
 		int sum =0;
 		 String[] numbers = Numbers.split(",");
 		 for(String s: numbers) 
-			 sum=sum+Integer.parseInt(s);
+			 sum = sum + Integer.parseInt(s);
 		 return sum;
 		
 	}
 	
 	public static void main (String args[]) {
-	try {
+
 		String numberString = "1,2";
-		String numberString2 = "";
+		String numberString2 = ""; 
 
 		StringCalculator sc = new StringCalculator();
+		//Testing INputs
 		System.out.println(sc.Add(numberString)) ;
 		System.out.println(sc.Add(numberString2)) ;
-		
-		
-		}
-	catch(Exception e)
-	{
-		System.out.println("0");
 	}
-}
 }
